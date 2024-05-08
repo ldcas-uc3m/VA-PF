61b0693e3b910b24efd11b42cd8a339059fd241e
ldcas-uc3m <100429021@alumnos.uc3m.es>
2024-05-08 20:08:04 +0200

    one last thing

--- zip.sh
+++ zip.sh
@@ -17,7 +17,7 @@ cp report/report.pdf .
 
 # zip it (excluding useless stuff)
 echo "Zipping..."
-zip -r $OUTFILE . -x zip.sh report/\* \*.git\* img/\* data/\* .venv/\* .vscode/\* README.md LICENSE requirements.txt results/\* \*__MACOSX/\*
+zip -r $OUTFILE . -x zip.sh report/\* \*.git\* img/\* data/\* .venv/\* .vscode/\* README.md LICENSE requirements.txt results/\* *__MACOSX/\* *__pycache__/\
 
 # cleanup
 echo "Cleaning up..."
